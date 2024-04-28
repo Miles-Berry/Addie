435728589d88b9851928ddc8d99de62dd08358e4
Miles Berry <70924215+Miles-Berry@users.noreply.github.com>
2024-04-28 12:26:07 -0500

    Basic Timer

---
+++ pomo.js
@@ -0,0 +1,55 @@
+let timerText = document.querySelector(".timer-clock");
+let timerButton = document.querySelector(".timer-start");
+
+// Different Timer times
+let timerOptions = [
+    {
+        name: "focus",
+        duration: 25
+    },{
+        name: "small_break",
+        duration: 5
+    },{
+        name: "large_break",
+        duration: "30"
+    }
+]
+
+function startTimer(option) {
+    console.log("StartedTimer");
+
+    // Timer Variables
+    let min = timerOptions[option].duration;
+    let sec = -1;
+    // Timer operation
+    let timer = setInterval( () => {
+        sec--;
+        //Time checking
+        if (sec < 0) {
+            if (min > 0) {
+                //Reset seconds and reduce minutes
+                sec = 59;
+                min--;
+            } else {
+                //Timer ended
+                clearInterval(timer);
+            }
+        }
+
+        //Formatting for single digit numbers
+        let leadingZeroMinutes = "";
+        let leadingZeroSeconds = "";
+        if (min < 10) {
+            leadingZeroMinutes = "0";
+        }
+        if (sec < 10) {
+            leadingZeroSeconds = "0"
+        }
+
+        //Update text
+        timerText.innerHTML = leadingZeroMinutes + min + ":" + leadingZeroSeconds + sec;
+
+    }, 1000);
+}
+
+timerButton.onclick = startTimer;
